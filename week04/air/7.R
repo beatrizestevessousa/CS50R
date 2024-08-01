a4558461b27d37aa293b7ca7188ab0e0a50614dd
Beatriz Sousa <121554636+beatrizestevessousa@users.noreply.github.com>
2024-08-01 16:29:52 +0100

    Update 7.R

--- week04/air/7.R
+++ week04/air/7.R
@@ -1,8 +1,12 @@
 load("air.RData")
 
-air <- air|>
-  group_by(county) %>%
-  filter(emissions == max(emissions)) %>%
-  ungroup()
+air <- air %>%
+  filter(level_1 %in% c("Industrial Processes", "Miscellaneous Area Sources", "Mobile Sources", "Natural Sources",
+                        "Solvent Utilization", "Stationary Source Fuel Combustion",
+                        "Storage and Transport", "Waste Disposal, Treatment, and Recovery")) %>%
+  group_by(level_1, pollutant) %>%
+  summarize(emissions = sum(emissions), .groups = 'drop') %>%
+  rename(source = level_1) %>%
+  arrange(source, pollutant)
 
-save(air, file="5.RData")
+save(air, file="7.RData")
